c221d74a47779bde79343e161e9699a119f684b6
Kyano DM <81612475+KyanoBoi@users.noreply.github.com>
2023-04-26 23:22:12 +0200

    Add files via upload

---
+++ docs/js/graph4.js
@@ -0,0 +1,56 @@
+const firebaseConfig4 = {
+  apiKey: "AIzaSyCG-BjnGE1V3X_18oxB325QroJ4UNftec8",
+  authDomain: "weatherdata-a36d3.firebaseapp.com",
+  databaseURL: "https://weatherdata-a36d3-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "weatherdata-a36d3",
+  storageBucket: "weatherdata-a36d3.appspot.com",
+  messagingSenderId: "237528717792",
+  appId: "1:237528717792:web:7ef4f5049fbc381e04b385",
+  measurementId: "G-VTTYPK4W1N"
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig4);
+}
+
+const database4 = firebase.database();
+const fetchedData4 = database4.ref('graph/windd');
+const time4 = database4.ref('graph/time');
+
+// Get the container element
+const container = document.getElementById('wind-direction');
+
+// Attach a listener to the data and update the divs when the data changes
+fetchedData4.on('value', async (snapshot) => {
+  const values = snapshot.val();
+
+  // Clear the container before adding new divs
+  container.innerHTML = '';
+
+  // Retrieve the value of the "graph/time" reference
+  const timeSnapshot = await time4.once('value');
+  const timeValues = timeSnapshot.val();
+
+  // Loop through each value and create a new div and image for each one
+  for (const valueKey in values) {
+    if (values.hasOwnProperty(valueKey)) {
+      const value = values[valueKey];
+      const timeValue = timeValues[valueKey];
+
+      const div = document.createElement('div');
+      div.className = 'box';
+
+      const text = document.createElement('div');
+      const img = document.createElement('img');
+      
+     
+      text.innerHTML = `${value}<br><img class="Richting" src="img/${value}.png"><br>${timeValue}`;
+      div.appendChild(text);
+
+      
+    
+
+      container.appendChild(div);
+    }
+  }
+});
---
+++ docs/js/hover3.js
@@ -0,0 +1,42 @@
+const bigCard2 = document.querySelector('.big-card2');
+const smallCard2 = document.querySelector('.small-card2');
+
+// Define a function to handle the click event
+function handleCardClick2(event) {
+  const clickedCard2 = event.currentTarget;
+
+  // Check if the event target is not the card itself
+  if (event.target !== clickedCard2) {
+    return;
+  }
+
+  // If the clicked card is already big, do nothing
+  if (clickedCard2.classList.contains('big-card2')) {
+    return;
+  }
+
+  // Find the current big card
+  const currentBigCard2 = document.querySelector('.big-card2');
+
+  // Toggle the 'big-card' and 'small-card' class names on the clicked card
+  clickedCard2.classList.toggle('big-card2');
+  clickedCard2.classList.toggle('small-card2');
+
+  // Toggle the 'big-card' and 'small-card' class names on the current big card
+  currentBigCard2.classList.toggle('big-card2');
+  currentBigCard2.classList.toggle('small-card2');
+}
+
+// Add the click event listener to both cards
+function handleChartClick2(event) {
+  const clickedChart2 = event.currentTarget;
+  const clickedCard2 = clickedChart2.parentElement;
+
+  // Trigger a click event on the parent card element
+  clickedCard2.click();
+}
+
+const chartCanvases2 = document.querySelectorAll('canvas');
+chartCanvases2.forEach(canvas => {
+  canvas.addEventListener('click', handleChartClick2);
+});
