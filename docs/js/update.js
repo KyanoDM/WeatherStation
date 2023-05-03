61d802ee8dc306274172f325557f61aa4f454c66
Kyano DM <81612475+KyanoBoi@users.noreply.github.com>
2023-05-03 15:33:39 +0200

    Delete docs directory

--- docs/js/update.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const button = document.querySelector('.updbut');
-    const slider = document.querySelector('#slider');
-
-    button.addEventListener('click', () => {
-        const sliderValue = slider.value;
-        console.log(sliderValue);
-    });
-});
